ad7b62dece03cfd9bf50114f96aa6510db71b42c
Ansil Tech <87252403+ansiltech@users.noreply.github.com>
2021-09-15 14:36:19 +0530

    Create __main__.py

---
+++ userbot/__main__.py
@@ -0,0 +1,78 @@
+import sys
+
+import userbot
+from userbot import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
+
+from .Config import Config
+from .core.logger import logging
+from .core.session import wolfub
+from .utils import (
+    add_bot_to_logger_group,
+    ipchange,
+    load_plugins,
+    setup_bot,
+    startupmessage,
+    verifyLoggerGroup,
+)
+
+LOGS = logging.getLogger("WolfUserbot")
+
+print(userbot.__copyright__)
+print("Licensed under the terms of the " + userbot.__license__)
+
+cmdhr = Config.COMMAND_HAND_LER
+
+try:
+    LOGS.info("Starting Userbot")
+    catub.loop.run_until_complete(setup_bot())
+    LOGS.info("TG Bot Startup Completed")
+except Exception as e:
+    LOGS.error(f"{e}")
+    sys.exit()
+
+
+class CatCheck:
+    def __init__(self):
+        self.sucess = True
+
+
+Wolfcheck = WolfCheck()
+
+
+async def startup_process():
+    check = await ipchange()
+    if check is not None:
+        Wolfcheck.sucess = False
+        return
+    await verifyLoggerGroup()
+    await load_plugins("plugins")
+    await load_plugins("assistant")
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+    print("Yay your userbot is officially working.!!!")
+    print(
+        f"Congratulation, now type {cmdhr}alive to see message if wolfub is live\
+        \nIf you need assistance, head to https://t.me/ansiltechteam"
+    )
+    print("➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖")
+    await verifyLoggerGroup()
+    await add_bot_to_logger_group(BOTLOG_CHATID)
+    if PM_LOGGER_GROUP_ID != -100:
+        await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
+    await startupmessage()
+    Catcheck.sucess = True
+    return
+
+
+catub.loop.run_until_complete(startup_process())
+
+
+if len(sys.argv) not in (1, 3, 4):
+    catub.disconnect()
+elif not Catcheck.sucess:
+    if HEROKU_APP is not None:
+        HEROKU_APP.restart()
+else:
+    try:
+        catub.run_until_disconnected()
+    except ConnectionError:
+        pass
